984389bbfef510cd314fa149959313c152ebd835
kasper084 <kasper084@gmail.com>
2020-02-11 04:30:43 +0200

    start

---
+++ src/main/java/times/App.java
@@ -0,0 +1,7 @@
+package times;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/times/AppTest.java
@@ -0,0 +1,20 @@
+package times;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
